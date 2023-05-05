1edc5930cfc4c3e8261e39eca7925c591cac9db1
Bruno Barros <brunog.barros@pm.me>
2023-05-04 23:07:15 -0300

    classe ship, laser e mostrando na tela

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
 import pygame
 import sys
 
-#basic setup
+
+class Ship(pygame.sprite.Sprite):
+
+    def __init__(self, groups):
+        # init parent class
+        super().__init__(groups)
+        # surface
+        self.image = pygame.image.load('./graphics/ship.png').convert_alpha()
+        # rect
+        self.rect = self.image.get_rect(
+            center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT/2))
+
+
+class Laser(pygame.sprite.Sprite):
+
+    def __init__(self, groups, pos: tuple):
+        super().__init__(groups)
+        self.image = pygame.image.load('./graphics/laser.png').convert_alpha()
+        self.rect = self.image.get_rect(
+            midbottom=pos)
+
+
+# basic setup
 pygame.init()
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+background_surf = pygame.image.load('./graphics/background.png').convert()
 clock = pygame.time.Clock()
 pygame.display.set_caption("Metor Shooter V2")
-#basic setup
+# end basic setup
 
-#game loop
+# sprites groups
+spaceship_group = pygame.sprite.Group()
+laser_group = pygame.sprite.Group()
+
+# sprite creation
+ship = Ship(spaceship_group)
+laser = Laser(laser_group, (50, 50))
+
+# game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #delta time - to mantain FPS
+    # delta time - to mantain FPS
     dt = clock.tick() / 1000
+    # background
+    display_surface.blit(background_surf, (0, 0))
 
-    #draw frames
+    # graphics
+    spaceship_group.draw(display_surface)
+    laser_group.draw(display_surface)
+
+    # draw frames
     pygame.display.update()
