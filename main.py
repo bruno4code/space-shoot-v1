fbcc3da141f961a98f797464e04e81729f4ae316
Bruno Barros <brunog.barros@pm.me>
2023-05-05 22:03:24 -0300

    Updating sprites

--- main.py
+++ main.py
@@ -12,6 +12,28 @@ class Ship(pygame.sprite.Sprite):
         # rect
         self.rect = self.image.get_rect(
             center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT/2))
+        self.can_shoot = True
+        self.shoot_time = None
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+
+    def input_position(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def update(self):
+        self.laser_timer()
+        self.laser_shoot()
+        self.input_position()
 
 
 class Laser(pygame.sprite.Sprite):
@@ -46,10 +68,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     # delta time - to mantain FPS
     dt = clock.tick() / 1000
     # background
     display_surface.blit(background_surf, (0, 0))
+
+    # update sprites
+    spaceship_group.update()
 
     # graphics
     spaceship_group.draw(display_surface)
